1d203ce1cb30fb354e227a4ed5319cc6d49a07fc
Matheus Lasserre <matheuslasserre3@gmail.com>
2023-06-08 01:55:32 -0300

    Well... Enums in rust are great

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 
 
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+impl Color {
+    fn is_green(&self) -> bool {
+        if let Color::Green = self {
+            return true;
+        }
+        return false;
+    }
+
+    fn is_green_parts(&self) -> bool {
+        match self {
+            Color::Red => return false,
+            Color::Green => return false,
+            Color::Blue => return true,
+        }
+    }
+}
+
+fn print_color (color: Color) {
+    match color {
+        Color::Red => println!("Red"),
+        Color::Green => println!("Green"),
+        Color::Blue => println!("Blue"),
+    }
+}
+
+
 fn main() {
-    std::fs::read_to_string("lines")
-    .unwrap()
-    .lines()
-    .enumerate()
-    .filter(|(idx, _)| idx % 2 == 0)
-    .skip(2)
-    .take(2)
-    .for_each(|(_, str)| println!("{}", str));
     
+    let foo = Color::Green;
+
+    foo.is_green();
 
 }
+
+
